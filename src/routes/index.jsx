334ad6470f4dd63fb38055c406d259b8482c0b3b
Syed-Ali-Murtaza-Rizvi <murtazarizvi407@gmail.com>
2025-11-17 19:04:50 +0500

    signup removed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,8 @@
 import {createBrowserRouter} from 'react-router-dom'
 import App from "../App"
 import Login from "../pages/Login"
-import Signup from '../pages/Signup'
+import TeacherDashboard from '../pages/TeacherDashboard'
+import StudentDashboard from '../pages/StudentDashboard'
 
 
 const router=createBrowserRouter([
@@ -10,15 +11,20 @@ const router=createBrowserRouter([
         element: <App/>,
         children:[
             {
-                path:"signup",
-                element:<Signup/>
-            },
-            {
                 path:"login",
                 element:<Login/>
+            },
+            {
+                path:"teacher",
+                element:<TeacherDashboard/>
+            },
+            {
+                path:"student",
+                element:<StudentDashboard/>
             }
         ]
-    }
+    },
+    
 ])
 
 export default router
