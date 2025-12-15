2007164f67ad9e939c20ca31b044da6ad8e542e4
emanshahid123 <emanshahid189@gmail.com>
2025-12-15 23:20:38 +0500

    DONE

--- src/components/admin/AdminHeader.jsx
+++ src/components/admin/AdminHeader.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 
 const AdminHeader = ({ tab, setTab }) => {
-  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
   return (
     <div className="admin-header">
       <div className="admin-left">
@@ -10,9 +9,7 @@ const AdminHeader = ({ tab, setTab }) => {
 </div>
         <div>
           <h2>Admin Dashboard</h2>
-         <p className="muted">
-            Welcome, {currentUser?.adminName || "Administrator"}
-          </p>
+          <p className="muted">Welcome, Administrator</p>
         </div>
       </div>
 
